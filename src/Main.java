9d747f06db381241b986447953105fdc50d67372
J. Pablo Pérez Trabucco <jpperez@ciudadanointeligente.org>
2012-08-10 12:44:00 -0400

    Adds a basic main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import org.accesointeligente.server.Scheduler;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scheduler scheduler = Scheduler.getInstance();
+		scheduler.run();
+	}
+}
